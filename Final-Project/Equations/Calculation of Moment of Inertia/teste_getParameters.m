9e8d0c22381032863fb49a4531221cf6e1f4ee32
Erivelton <erivelton.gualter@gmail.com>
2021-03-21 02:02:55 -0400

    added code from undergraduate plus modifications

---
+++ Final-Project/Equations/Calculation of Moment of Inertia/teste_getParameters.m
@@ -0,0 +1,26 @@
+clear all
+close all
+
+H  = 1.72;       % [m]
+Px = 0;
+Py = -0.231;
+
+% If you desire to enter with the mass value, just uncomment here. In
+% contrast, the mass according to BMI (Font: Brasil-2003) will be used.
+%
+M  = 75;   
+[q, J] = parameters2(H, Px, Py, M);
+
+disp(q);
+disp(J);
+
+%[q, J] = parameters(H, Px, Py);
+
+phi0 = atan2(q(2,1),q(1,1));
+
+w = sys_wheelchair;
+L = sqrt(q(1,1)*q(1,1) + q(2,1)*q(2,1));
+
+% tau_lim = (w.Mcr + 75 + w.JR/(w.R^2))*w.R*L*cos(phi0)*w.g/(L*sin(phi0)+w.R)
+% L*cos(phi0)*w.g/(L*sin(phi0)+w.R)
+
